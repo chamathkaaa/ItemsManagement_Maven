4777263d729bdf69891b40ba5a1870c12208f653
chamathkaaa <uviniwijesinghe@gmail.com>
2020-04-15 00:01:32 +0530

    connection

--- ItemService/src/model/Item.java
+++ ItemService/src/model/Item.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Item {
 
+	// A common method to connect to the DB
+	private Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
 }
